c0384808c0ffe170342cf2238cf388ef0bdfab91
Hussam Aldarwish <hussam.aldarwish@hotmail.com>
2024-11-27 19:35:47 +0000

    feat: :sparkles: add JSON server configuration with sample data and API proxy rewrites

---
+++ herlpers/env.ts
@@ -0,0 +1,4 @@
+// Environment variables
+
+// API URL
+export const API_URL = process.env.API_URL || 'http://localhost:5000';
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
+import { API_URL } from './herlpers/env';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // Proxy not found API routes to the API (JSON-SERVER)
+      {
+        source: '/api/:path*',
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
