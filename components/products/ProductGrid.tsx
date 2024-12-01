3e8c916bf494bf2898759fec163ea5df815f0590
Hussam Aldarwish <hussam.aldarwish@hotmail.com>
2024-12-02 00:28:31 +0300

    feat: :sparkles: add boot notification for prolonged loading times in ProductGrid component

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -3,7 +3,7 @@
 import { getProductsAction } from '@/actions/productActions';
 import { PaginatedResponse, Product } from '@/types/custom-types';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FaSpinner } from 'react-icons/fa6';
 import ProductCard from './ProductCard';
 
@@ -40,12 +40,37 @@ const ProductGrid: React.FC<ProductGridProps> = ({ q }) => {
     if (lastElementRef.current) observerRef.current.observe(lastElementRef.current);
   }, [isFetchingNextPage, hasNextPage, fetchNextPage]);
 
+  const [showBootNotification, setShowBootNotification] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+
+    if (isLoading) {
+      timer = setTimeout(() => {
+        setShowBootNotification(true);
+      }, 5000); // Show notification after 5 seconds of loading
+    } else {
+      setShowBootNotification(false); // Hide notification when loading is complete
+    }
+
+    return () => {
+      if (timer) clearTimeout(timer); // Cleanup timeout
+    };
+  }, [isLoading]);
+
   return (
     <div className='container mx-auto px-4'>
       <h1 className='text-4xl font-extrabold text-primary text-center mb-12'>
         Discover Our Exclusive Products
       </h1>
-      {isLoading && <p className='text-center text-lg'>Loading...</p>}
+      {(isLoading || showBootNotification) && (
+        <div className='py-8 bg-light text-dark text-center rounded-lg shadow-lg flex items-center justify-center gap-2 mb-4'>
+          <FaSpinner className='animate-spin' />
+          {showBootNotification
+            ? 'Loading data is taking longer than usual. The backend server may be booting up, please wait...'
+            : 'Loading products...'}
+        </div>
+      )}
       {error && <p className='text-center text-lg text-red-500 mb-4'>{error.message}</p>}
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
         {data?.pages.map((page, i) => (
