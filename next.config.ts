8b28b88fd798a735c6f16dc94cc0f7f3979f738a
Hussam Aldarwish <hussam.aldarwish@hotmail.com>
2024-12-05 11:16:00 +0000

    feat: add redirect from home page to products in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
